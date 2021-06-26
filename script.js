ca75606b84889bf047a3e8234eb36c46277c4cf2
JPablo73 <82916926+JPablo73@users.noreply.github.com>
2021-06-26 11:48:39 -0700

    changed title, subtitle and switched cards

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// ivanM added
+// Time Zone
 
 function time() {
 	var timeZone = "http://worldtimeapi.org/api/timezone"
@@ -24,27 +24,30 @@ time()
 // Los Angeles
 
 
-const settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://google-translate1.p.rapidapi.com/language/translate/v2",
-	"method": "POST",
-	"headers": {
-		"content-type": "application/x-www-form-urlencoded",
-		"accept-encoding": "application/gzip",
-		"x-rapidapi-key": "731087d542msh4e208bf26768a2fp108983jsna4e8d1d5ba68",
-		"x-rapidapi-host": "google-translate1.p.rapidapi.com"
-	},
-	"data": {
-		"q": "city",
-		"target": "ar",
-		"source": "en"
-	}
-};
 
-$.ajax(settings).done(function (response) {
-	console.log(response);
-});
+// Word Translator
+
+// const settings = {
+// 	"async": true,
+// 	"crossDomain": true,
+// 	"url": "https://google-translate1.p.rapidapi.com/language/translate/v2",
+// 	"method": "POST",
+// 	"headers": {
+// 		"content-type": "application/x-www-form-urlencoded",
+// 		"accept-encoding": "application/gzip",
+// 		"x-rapidapi-key": "731087d542msh4e208bf26768a2fp108983jsna4e8d1d5ba68",
+// 		"x-rapidapi-host": "google-translate1.p.rapidapi.com"
+// 	},
+// 	"data": {
+// 		"q": "city",
+// 		"target": "ar",
+// 		"source": "en"
+// 	}
+// };
+
+// $.ajax(settings).done(function (response) {
+// 	console.log(response);
+// });
 
 //word translator- languages:
 // const languages = {
