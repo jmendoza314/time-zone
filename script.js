810a85dce00cdf2d96cbe5b8b97207d38cc053a4
jeremym <jeremymendoza05@yahoo.com>
2021-06-28 06:00:13 -0700

    worked on manipulation of timezone string

--- script.js
+++ script.js
@@ -3,29 +3,23 @@ var dateTime = document.querySelector('.input');
 var currTime = document.querySelector('.time')
 currTime.innerText = (new Date().toLocaleString('en-US'))
 
-// When submit button is clicked translation API will run
-submitBtn.addEventListener("click", () => {
-	function time() {
-		var sydney = document.querySelector('#sydney');
-		var tokyo = document.querySelector('#tokyo');
-		var dubai = document.querySelector('#dubai');
-		var pyongyang = document.querySelector('#pyongyang')
-		var moscow = document.querySelector('#moscow')
-		var cairo = document.querySelector('#cairo')
-		var johannesburg = document.querySelector('#johannesburg')
-		var paris = document.querySelector('#paris')
-		var london = document.querySelector('#london')
-		var saopaulo = document.querySelector('#saopaulo')
-		var newyork = document.querySelector('#newyork')
-		var panama = document.querySelector('#panama')
-		var denver = document.querySelector('#denver')
-		var mexicocity = document.querySelector('#mexicocity')
-		var losangeles = document.querySelector('#losangeles')
-		var anchorage = document.querySelector('#anchorage')
+var sydney = document.querySelector('#sydney');
+var tokyo = document.querySelector('#tokyo');
+var dubai = document.querySelector('#dubai');
+var pyongyang = document.querySelector('#pyongyang')
+var moscow = document.querySelector('#moscow')
+var cairo = document.querySelector('#cairo')
+var johannesburg = document.querySelector('#johannesburg')
+var paris = document.querySelector('#paris')
+var london = document.querySelector('#london')
+var saopaulo = document.querySelector('#saopaulo')
+var newyork = document.querySelector('#newyork')
+var panama = document.querySelector('#panama')
+var denver = document.querySelector('#denver')
+var mexicocity = document.querySelector('#mexicocity')
+var losangeles = document.querySelector('#losangeles')
+var anchorage = document.querySelector('#anchorage')
 
-	}
-	time()
-});
 
 document.addEventListener("input", handleSelect)
 function handleSelect(ev) {
@@ -38,21 +32,17 @@ function handleSelect(ev) {
 		})
 		.then(function (data) {
 			console.log(data.datetime)
-			dateTime.value = data.datetime
+			dateTime.value = data.datetime.slice(0, 19).replace('T', '  ');
 		})
 }
-function selectedData(ev) {
-	let theInput = ev.target
-
-}
 
 
-// var timeZone = "http://worldtimeapi.org/api/timezones"
-// 	fetch(timeZone)
+
+// var travel = "https://api.sygictravelapi.com/1.1/en"
+// 	fetch(travel)
 // 		.then(function (response) {
 // 			return response.json();
 // 		})
 // 		.then(function (data) {
-// 			dateTime.value = data
 // 			console.log(data);
 // 		})
